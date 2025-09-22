f3f546e61c8a109854aa748520e80fb9ef463d9c
axwell0 <messaimahdi@tbs.u-tunis.tn>
2025-09-22 01:34:26 +0100

    fix and improve dockerfiles and compose

---
+++ deploy/ops/postgres/init/init.sql
@@ -0,0 +1,7 @@
+-- Create additional databases for microservices
+CREATE DATABASE vehicle;
+CREATE DATABASE trips;
+CREATE DATABASE SmartS;
+CREATE DATABASE maintenance;
+CREATE DATABASE geofence;
+CREATE DATABASE billing;
